081f5b277b9ddd5a8dc66a4842144d12618715b0
yahyaouarach1999-creator <yahya.ouarach1999@gmail.com>
2026-02-10 14:49:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-name, auth_status, username = authenticator.login("Login", "main")
+name, auth_status, username = authenticator.login(location="main")
 
 if auth_status is False:
     st.error("Invalid credentials")
