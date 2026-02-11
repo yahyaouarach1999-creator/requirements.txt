a0823b9d6950103576aab5d5b530253b202ada87
yahyaouarach1999-creator <yahya.ouarach1999@gmail.com>
2026-02-11 12:18:24 +0100

    Update hash_generator.py

--- hash_generator.py
+++ hash_generator.py
@@ -1,6 +1,1 @@
-import streamlit_authenticator as stauth
 
-passwords = ["admin123"]  # change if you want a different password
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-print(hashed_passwords[0])
